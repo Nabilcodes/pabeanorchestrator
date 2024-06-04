6416a907899cc88829777261931e138b34e08b55
Fauzan Andri <fauzanandri11@gmail.com>
2024-06-04 13:27:10 +0700

    sesuikan orchestrator untuk dipanggil dari frontend

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import httpx
 import json
 from fastapi import FastAPI
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import BillingRequest
 
@@ -34,6 +35,16 @@ class OrchestrationService:
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Initialize service clients
 service_client1 = ServiceClient(base_url="http://127.0.0.1:8001")
 # service_client2 = ServiceClient(base_url="http://example-service2")
@@ -42,6 +53,9 @@ service_client1 = ServiceClient(base_url="http://127.0.0.1:8001")
 # orchestration_service = OrchestrationService(service_clients=[service_client1, service_client2])
 orchestration_service = OrchestrationService(service_clients=[service_client1])
 
+@app.options("/orchestrate-get-billing")
+def options():
+    return 200
 @app.post("/orchestrate-get-billing")
 def start_orchestration(billingReq: dict):
     result = orchestration_service.orchestrate(data=billingReq) 
--- models.py
+++ models.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 class BillingRequest(BaseModel):
     billed_name : str
     billed_email : str
-    value : int
+    nilai_fob : int
+    nilai_pabean : int
